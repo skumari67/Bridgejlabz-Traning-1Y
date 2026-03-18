626e4c3f6dbcb9bfbb0def350144db8442607804
skumari67 <skumari11oct@gmail.com>
2026-03-18 14:45:38 +0530

    Delete java-package directory

--- java-package/Java-Packages/src/com/access/one/AccessOneHelper.java
+++
@@ -1,6 +0,0 @@
-package com.access.one;
-public class AccessOneHelper {
-    public String getSourceName() {
-        return "AccessOne";
-    }
-}
--- java-package/Java-Packages/src/com/school/util/DataFormatter.java
+++
@@ -1,6 +0,0 @@
-package com.school.util;
-public class DataFormatter {
-    public String formatRollNumber(int rollNumber) {
-        return "ROLL-" + rollNumber;
-    }
-}
